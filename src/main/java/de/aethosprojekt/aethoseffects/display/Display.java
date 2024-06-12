756458b139442a98be8b466e2de3325a2d70ed76
Tyrael <xykingofskillyx@gmail.com>
2024-06-12 22:43:21 +0200

    Remove Displays

--- src/main/java/de/aethosprojekt/aethoseffects/display/Display.java
+++ src/main/java/de/aethosprojekt/aethoseffects/display/Display.java
@@ -1,13 +1,9 @@
 package de.aethosprojekt.aethoseffects.display;
 
-
 import org.bukkit.entity.LivingEntity;
 import org.jetbrains.annotations.NotNull;
 
 public interface Display {
-
-
     void display(@NotNull LivingEntity entity);
-
 
 }
