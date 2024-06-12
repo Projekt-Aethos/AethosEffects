aa398b8379be82e9421c7f09289731278b6e2905
Tyrael <xykingofskillyx@gmail.com>
2024-06-12 23:35:27 +0200

    Refactor Names

--- src/main/java/de/aethosprojekt/aethoseffects/entity/BossBarSerializer.java
+++ src/main/java/de/aethosprojekt/aethoseffects/entity/BossBarSerializer.java
@@ -7,10 +7,10 @@ import java.util.List;
 public class BossBarSerializer {
 
 
-    private static final String MAGIC = "ꈁꈇꈅꈄꈁ";
+    private static final String MAGIC_STRING = "ꈁꈇꈅꈄꈁ";
 
     public static @NotNull String serialize(ActiveEffect effect) {
-        return MAGIC + effect.getType().getIcon() + String.format("%02d:%02d", effect.getRemainingTicks() / 1200, (effect.getRemainingTicks() % 1200) / 20);
+        return MAGIC_STRING + effect.getType().getIcon() + String.format("%02d:%02d", effect.getRemainingTicks() / 1200, (effect.getRemainingTicks() % 1200) / 20);
     }
 
     public static @NotNull String serialize(List<ActiveEffect> effects) {
